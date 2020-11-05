504cc83ca17103946e93fb02667288a2eafd5359
Max Hobday <mhobday@unomaha.edu>
2020-11-05 15:26:06 -0600

    The Game is Perfect
    Started movement and thought this was funny

---
+++ Racoon Game/Assets/Scripts/RacoonMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacoonMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    [SerializeField]
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") *moveSpeed);
+    }
+
+
+}
