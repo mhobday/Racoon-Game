7f4c2df1ea3080d9e185c0f01435a2da9c6b61a2
Max Hobday <mhobday@unomaha.edu>
2020-11-08 09:04:03 -0600

    Attempted animations

--- Racoon Game/Assets/Scripts/RacoonMovement.cs
+++ Racoon Game/Assets/Scripts/RacoonMovement.cs
@@ -8,16 +8,21 @@ public class RacoonMovement : MonoBehaviour
     public float moveSpeed;
     [SerializeField]
     private Rigidbody rigidbody;
+    [SerializeField]
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = this.gameObject.GetComponent<Rigidbody>();
+        animator = this.gameObject.GetComponent<Animator>();
+        animator.Play("Idle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") *moveSpeed);
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        
     }
 
 
