abe6de0b13553757ae4bb2ddc3e1d0e1d45372fc
waramsey <waramsey@unomaha.edu>
2020-11-10 17:20:46 -0700

    Garbage Rain Update

---
+++ Racoon Game/Assets/Scripts/garbageRain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class garbageRain : MonoBehaviour
+{
+    public Queue garbage = new Queue();
+    public GameObject[] garbageOptions;
+    public int garbageMax;
+    public float garbageRate;
+    private int garbageCount = 0;
+    private float timeElapsed = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > garbageRate) {
+            garbage.Enqueue(
+                Instantiate(
+                    garbageOptions[(int) Mathf.Floor(Random.Range(0.0f, 0.999f) * garbageOptions.Length)],
+                    this.gameObject.transform.position,
+                    Random.rotation
+                )
+            );
+            garbageCount++;
+            timeElapsed = 0;
+            if (garbageCount > garbageMax) {
+                Destroy((GameObject) garbage.Dequeue());
+            }
+        }
+    }
+}
---
+++ Racoon Game/Assets/sideToSide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sideToSide : MonoBehaviour
+{
+    public int distance;
+    private int positionTracker = 0;
+    private bool right = true;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (right && positionTracker < distance) {
+            Vector3 temp = this.gameObject.transform.position;
+            temp.x += 1.0f;
+            this.gameObject.transform.position = temp;
+            positionTracker++;
+        } else if (!right && positionTracker < distance) {
+            Vector3 temp = this.gameObject.transform.position;
+            temp.x -= 1.0f;
+            this.gameObject.transform.position = temp;
+            positionTracker++;
+        } else {
+            right = !right;
+            positionTracker = 0;
+        }
+    } 
+}
