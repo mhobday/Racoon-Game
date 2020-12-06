6c6a8d70cd1fbae601a4be3435e604857fbfbf5d
hawk588 <gdhawk588@gmail.com>
2020-12-06 17:49:58 -0600

    Doors can open

--- Racoon Game/Assets/Scripts/HouseScripts/DoorScript.cs
+++ Racoon Game/Assets/Scripts/HouseScripts/DoorScript.cs
@@ -7,22 +7,28 @@ public class DoorScript : MonoBehaviour
     // Start is called before the first frame update
     public bool locked = false;
 
+    public Vector3 position;
+
     void Start()
     {
-        
+        float rotateAxisx = this.gameObject.GetComponent<MeshRenderer>().bounds.size.x / 2;
+        this.position = this.gameObject.transform.position;
+        this.position.x += rotateAxisx;
+        this.Open();
     }
 
     // Update is called once per frame
     void Open()
     {
+        
         if(!locked)
         {
-            
+            this.gameObject.transform.RotateAround(this.position ,Vector3.up, 90);
         }
     }
 
     void close()
     {
-        
+        this.gameObject.transform.RotateAround(this.position ,Vector3.up, -90);
     }
 }
