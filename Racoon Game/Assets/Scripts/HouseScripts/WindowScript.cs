a027b17b0bd3db9bb6fd4ba3b1fdd020b5b67113
hawk588 <gdhawk588@gmail.com>
2020-12-06 15:01:04 -0600

    WindowBreak method

--- Racoon Game/Assets/Scripts/HouseScripts/WindowScript.cs
+++ Racoon Game/Assets/Scripts/HouseScripts/WindowScript.cs
@@ -3,26 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WindowScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
 
-    // Update is called once per frame
+    public GameObject brokenWindow;
     void open()
     {
-
+        this.gameObject.SetActive(false);
     }
 
     void close()
     {
-
+        this.gameObject.SetActive(true);
     }
 
     void breakWindow()
     {
-
+        float x = this.gameObject.transform.localScale.x;
+        float y = this.gameObject.transform.localScale.y;
+        float z = this.gameObject.transform.localScale.z;
+        float yRotation = this.gameObject.transform.rotation.y;
+        GameObject newWindow = Instantiate(brokenWindow, this.gameObject.transform.position, Quaternion.identity);
+        if (yRotation != 0)
+        {
+            newWindow.transform.Rotate(0, 90, 0); 
+        }
+        
+        Vector3 change = new Vector3(x, y, 1);
+        newWindow.transform.localScale = change;
+        Destroy(this.gameObject);
     }
 }
