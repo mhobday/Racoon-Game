61679d8e9eafea912b4084ac1c3052332e45c360
waramsey <waramsey@unomaha.edu>
2020-12-10 15:57:23 -0700

    Working on Animations

---
+++ Racoon Game/Assets/Scripts/MapScripts/enemyLoc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyLoc : MonoBehaviour
+{
+    private void OnCollision(Collision other) {
+        Debug.Log("CollisionEnter");
+        if (other.gameObject.tag == "Enemy") {
+            other.gameObject.GetComponent<Animator>().SetTrigger("AtStart");
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.tag == "Enemy") {
+            other.gameObject.GetComponent<Animator>().ResetTrigger("AtStart");
+        }   
+    }
+}
