5f8388a35ed134236a143f92cd955f4dbbb09170
Max Hobday <mhobday@unomaha.edu>
2020-12-11 13:22:31 -0600

    Fix mouselock

--- Racoon Game/Assets/Scripts/MouseLock.cs
+++ Racoon Game/Assets/Scripts/MouseLock.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MouseLock : MonoBehaviour
 {
     private void Start() {
-        Lock();
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (sceneName == "MainMenu")
+        {
+            Unlock();
+        }
+        else
+        {
+            Lock();
+        }
     }
 
     public void Lock() {
