d69a7df6d537d38302778f8dd6d406b8dde86429
waramsey <waramsey@unomaha.edu>
2020-12-10 19:38:17 -0700

    Added Audio Clips

--- Racoon Game/Assets/Scripts/RaccoonScripts/racoonDeath.cs
+++ Racoon Game/Assets/Scripts/RaccoonScripts/racoonDeath.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class racoonDeath : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
+    public AudioClip GameOver;
+    public Camera camera;
+
+    private void OnCollisionEnter(Collision other) {
+
         Debug.Log("test");
         if(other.gameObject.CompareTag("Enemy"))
         {
             this.gameObject.GetComponent<Animation>().Play("Death");
             this.gameObject.GetComponent<racoonMovement>().enabled = false;
+            StartCoroutine(EndGame(3.0f));
         }
+    }
+
+    IEnumerator EndGame(float waitTime) {
+        AudioClip temp = camera.GetComponent<AudioSource>().clip;
+        camera.GetComponent<AudioSource>().clip = GameOver;
+        camera.GetComponent<AudioSource>().Play();
+        camera.GetComponent<AudioSource>().loop = false;
+        yield return new WaitForSeconds(waitTime);
+        camera.GetComponent<AudioSource>().clip = temp;
+        camera.GetComponent<AudioSource>().loop = true;
+        SaveLoad.Save();
+        MenuLoader.GoToMenu(MenuName.Main);
     }
 }
---
+++ Racoon Game/Assets/Spotted.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotted : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponentInParent<AudioSource>().Play();
+    }
+}
